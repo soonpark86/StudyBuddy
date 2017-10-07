9858cfc08ea7bd34db98e6e458ebb8a71fcf3e22
Soon Park <soonpark86@gmail.com>
2017-10-06 23:05:20 -0500

    module updates

---
+++ StudyBuddy/Helpers/GeneralHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace StudyBuddy.Helpers
+{
+    public class GeneralHelper
+    {
+        public static string GetRDSConnectionString()
+        {
+            var appConfig = ConfigurationManager.AppSettings;
+
+            string dbname = appConfig["StudyBuddy"];
+
+            if (string.IsNullOrEmpty(dbname)) return null;
+
+            string username = appConfig["soonpark86"];
+            string password = appConfig["dh119119"];
+            string hostname = appConfig["aakhaxjufw2s5r.c5jifixvwcrz.us-east-2.rds.amazonaws.com"];
+            string port = appConfig["1433"];
+
+            return "Data Source=" + hostname + ";Initial Catalog=" + dbname + ";User ID=" + username + ";Password=" + password + ";";
+        }
+    }
+}
